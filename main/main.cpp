77c2486dd1087b0f09de3d871c1d88c0fb55419c
dariatsyganovaa <dcyganova857@gmail.com>
2025-09-22 19:36:13 +0300

    code extraction into functions

--- main/main.cpp
+++ main/main.cpp
@@ -2,10 +2,155 @@
 #include <iomanip>
 
 #define MATRIX
-
 #ifdef MATRIX
 
 #include "../lib_triangle_matrix/triangle_matrix.h"
+
+void start_add_matrix(Matrix<int>& matrix_1, int N1, int M1) {
+    system("cls");
+    std::cout << "==== MATRIX ADDITION ====" << std::endl;
+
+    int N2, M2;
+    while (1) {
+        std::cout << "Enter the size of 2-nd matrix: ";
+        std::cin >> N2 >> M2;
+
+        if (N1 != N2 || M1 != M2) {
+            std::cout << "Error! The sizes of the matrices must match! ";
+            continue;
+        }
+        break;
+    }
+    Matrix<int> matrix_2(N2, M2);
+    matrix_2.input_matrix();
+
+    Matrix<int> result = matrix_1 + matrix_2;
+    result.print_matrix();
+    system("pause");
+}
+
+void start_sub_matrix(Matrix<int>& matrix_1, int N1, int M1) {
+    system("cls");
+    std::cout << "==== MATRIX SUBTRACTION ====" << std::endl;
+
+    int N2, M2;
+    while (1) {
+        std::cout << "Enter the size of 2-nd matrix: ";
+        std::cin >> N2 >> M2;
+
+        if (N1 != N2 || M1 != M2) {
+            std::cout << "Error! The sizes of the matrices must match! ";
+            continue;
+        }
+        break;
+    }
+    Matrix<int> matrix_2(N2, M2);
+    matrix_2.input_matrix();
+
+    Matrix<int> result = matrix_1 - matrix_2;
+    result.print_matrix();
+    system("pause");
+}
+
+void start_mult_matrix(Matrix<int>& matrix_1, int N1, int M1) {
+    system("cls");
+    std::cout << "==== MATRIX MULTIPLICATION ====" << std::endl;
+
+    int N2, M2;
+    while (1) {
+        std::cout << "Enter the size of 2-nd matrix: ";
+        std::cin >> N2 >> M2;
+        if (M1 != N2) {
+            std::cout << "Error! The number of cols in the 1-st matrix must match the number of rows in the 2-nd matrix! ";
+            continue;
+        }
+        break;
+    }
+    Matrix<int> matrix_2(N2, M2);
+    matrix_2.input_matrix();
+
+    Matrix<int> result = matrix_1 * matrix_2;
+    result.print_matrix();
+    system("pause");
+}
+
+void start_matrix_scalar_mult(Matrix<int>& matrix_1) {
+    system("cls");
+    std::cout << "==== MATRIX MULTIPLICATION BY A SCALAR ====" << std::endl;
+
+    int scalar;
+    std::cout << "Enter a scalar: ";
+    std::cin >> scalar;
+
+    Matrix<int> result = matrix_1 * scalar;
+    result.print_matrix();
+    system("pause");
+}
+
+void start_trans_matrix(Matrix<int>& matrix_1) {
+    system("cls");
+    std::cout << "==== MATRIX TRANSPOSITION ====" << std::endl;
+
+    matrix_1.trans();
+    matrix_1.print_matrix();
+    system("pause");
+}
+
+void start_add_tri_matrix(TriangleMatrix<int>& tri_matrix_1, int size) {
+    system("cls");
+    std::cout << "==== TRIANGULAR MATRIX ADDITION ====" << std::endl;
+
+    TriangleMatrix<int> tri_matrix_2(size);
+    tri_matrix_2.input_tri_matrix();
+
+    TriangleMatrix<int> result = tri_matrix_1 + tri_matrix_2;
+    result.print_tri_matrix();
+    system("pause");
+}
+
+void start_sub_tri_matrix(TriangleMatrix<int>& tri_matrix_1, int size) {
+    system("cls");
+    std::cout << "==== TRIANGULAR MATRIX SUBTRACTION ====" << std::endl;
+
+    TriangleMatrix<int> tri_matrix_2(size);
+    tri_matrix_2.input_tri_matrix();
+
+    TriangleMatrix<int> result = tri_matrix_1 - tri_matrix_2;
+    result.print_tri_matrix();
+    system("pause");
+}
+
+void start_mult_tri_matrix(TriangleMatrix<int>& tri_matrix_1, int size) {
+    system("cls");
+    std::cout << "==== TRIANGULAR MATRIX MULTIPLICATION ====" << std::endl;
+
+    TriangleMatrix<int> tri_matrix_2(size);
+    tri_matrix_2.input_tri_matrix();
+
+    TriangleMatrix<int> result = tri_matrix_1 * tri_matrix_2;
+    result.print_tri_matrix();
+    system("pause");
+}
+
+void start_tri_matrix_scalar_mult(TriangleMatrix<int>& tri_matrix_1) {
+    system("cls");
+    std::cout << "==== TRIANGULAR MATRIX MULTIPLICATION BY A SCALAR ====" << std::endl;
+
+    int scalar;
+    std::cout << "Enter a scalar: ";
+    std::cin >> scalar;
+
+    TriangleMatrix<int> result = tri_matrix_1 * scalar;
+    result.print_tri_matrix();
+    system("pause");
+}
+
+void start_type_tri_matrix(TriangleMatrix<int>& tri_matrix_1) {
+    system("cls");
+    std::cout << "==== TRIANGULAR MATRIX TYPE ====" << std::endl;
+    tri_matrix_1.status();
+    system("pause");
+}
 
 void MatrixOperations() {
     int N1, M1;
@@ -14,12 +159,13 @@ void MatrixOperations() {
     std::cin >> N1 >> M1;
     Matrix <int> matrix_1(N1, M1);
     matrix_1.input_matrix();
+    system("pause");
 
     while (1) {
         system("cls");
 
         std::cout << "==== MATRIX CALCULATOR ====" << std::endl;
-        std::cout << "Choose:\n1. add \n2. sub \n3. mult \n4. mult by a scalar \n5. transposition \n0. back to main menu\nYour: ";
+        std::cout << "Choose:\n1. Add \n2. Sub \n3. Mult \n4. Mult by a scalar \n5. Transposition \n0. Back to main menu\nYour: ";
 
         int user;
         std::cin >> user;
@@ -28,105 +174,25 @@ void MatrixOperations() {
 
         switch (user) {
         case 1: {
-            system("cls");
-            std::cout << "==== MATRIX ADDITION ====" << std::endl;
-
-            int N2, M2;
-            while (1) {
-                std::cout << "Enter the size of 2-nd matrix: ";
-                std::cin >> N2 >> M2;
-
-                if (N1 != N2 || M1 != M2) {
-                    std::cout << "Error! The sizes of the matrices must match! ";
-                    continue;
-                }
-                break;
-            }
-            Matrix<int> matrix_2(N2, M2);
-            matrix_2.input_matrix();
-
-            Matrix<int> result = matrix_1 + matrix_2;
-            result.print_matrix();
-            system("pause");
-
+            start_add_matrix(matrix_1, N1, M1);
             break;
         }
-            
         case 2: {
-            system("cls");
-            std::cout << "==== MATRIX SUBTRACTION ====" << std::endl;
-
-            int N2, M2;
-            while (1) {
-                std::cout << "Enter the size of 2-nd matrix: ";
-                std::cin >> N2 >> M2;
-
-                if (N1 != N2 || M1 != M2) {
-                    std::cout << "Error! The sizes of the matrices must match! ";
-                    continue;
-                }
-                break;
-            }
-            Matrix<int> matrix_2(N2, M2);
-            matrix_2.input_matrix();
-
-            Matrix<int> result = matrix_1 - matrix_2;
-            result.print_matrix();
-            system("pause");
-
+            start_sub_matrix(matrix_1, N1, M1);
             break;
-        }
-            
+        } 
         case 3: {
-            system("cls");
-            std::cout << "==== MATRIX MULTIPLICATION ====" << std::endl;
-
-            int N2, M2;
-            while (1) {
-                std::cout << "Enter the size of 2-nd matrix: ";
-                std::cin >> N2 >> M2;
-                if (M1 != N2) {
-                    std::cout << "Error! The number of cols in the 1-st matrix must match the number of rows in the 2-nd matrix! ";
-                    continue;
-                }
-                break;
-            }
-            Matrix<int> matrix_2(N2, M2);
-            matrix_2.input_matrix();
-
-            Matrix<int> result = matrix_1 * matrix_2;
-            result.print_matrix();
-            system("pause");
-
+            start_mult_matrix(matrix_1, N1, M1);
             break;
         }
-
         case 4: {
-            system("cls");
-            std::cout << "==== MATRIX MULTIPLICATION BY A SCALAR ====" << std::endl;
-
-            int scalar;
-            std::cout << "Enter a scalar: ";
-            std::cin >> scalar;
-
-            Matrix<int> result = matrix_1 * scalar;
-            result.print_matrix();
-            system("pause");
-
+            start_matrix_scalar_mult(matrix_1);
             break;
         }
-            
         case 5: {
-            system("cls");
-            std::cout << "==== MATRIX TRANSPOSITION ====" << std::endl;
-
-            matrix_1.trans();
-            matrix_1.print_matrix();
-            system("pause");
-
+            start_trans_matrix(matrix_1);
             break;
         }
-            
         default:
             std::cout << "Wrong input!\n";
             system("pause");
@@ -136,17 +202,18 @@ void MatrixOperations() {
 }
 
 void TriangleMatrixOperations() {
-    int size_1;
-    std::cout << "Enter the size of 1-st matrix: ";
-    std::cin >> size_1;
+    int size;
+    std::cout << "Enter the size of triangular matrix: ";
+    std::cin >> size;
 
-    TriangleMatrix<int> tri_matrix_1(size_1);
+    TriangleMatrix<int> tri_matrix_1(size);
     tri_matrix_1.input_matrix();
+    system("pause");
 
     while (1) {
         system("cls");
         std::cout << "==== TRIANGULAR MATRIX CALCULATOR ====" << std::endl;
-        std::cout << "Choose:\n1. add \n2. sub \n3. mult \n4. mult by a scalar \n5. status \n0. back to main menu\nYour: ";
+        std::cout << "Choose:\n1. Add \n2. Sub \n3. Mult \n4. Mult by a scalar \n5. Status \n0. Back to main menu\nYour: ";
 
         int user;
         std::cin >> user;
@@ -155,96 +222,23 @@ void TriangleMatrixOperations() {
 
         switch (user) {
         case 1: {
-            system("cls");
-            std::cout << "==== TRIANGULAR MATRIX ADDITION ====" << std::endl;
-
-            int size_2;
-            while (1) {
-                std::cout << "Enter the size of 2-nd matrix: ";
-                std::cin >> size_2;
-
-                if (size_1 != size_2) { 
-                    std::cout << "Error! The sizes of the matrices must match! ";
-                    continue;
-                }
-                break;
-            }
-            TriangleMatrix<int> tri_matrix_2(size_2);
-            tri_matrix_2.input_tri_matrix();
-
-            TriangleMatrix<int> result = tri_matrix_1 + tri_matrix_2;
-            result.print_tri_matrix();
-            system("pause");
-
+            start_add_tri_matrix(tri_matrix_1, size);
             break;
         }
         case 2: {
-            system("cls");
-            std::cout << "==== TRIANGULAR MATRIX SUBTRACTION ====" << std::endl;
-
-            int size_2;
-            while (1) {
-                std::cout << "Enter the size of 2-nd matrix: ";
-                std::cin >> size_2;
-
-                if (size_1 != size_2) { 
-                    std::cout << "Error! The sizes of the matrices must match! ";
-                    continue;
-                }
-                break;
-            }
-            TriangleMatrix<int> tri_matrix_2(size_2);
-            tri_matrix_2.input_tri_matrix();
-
-            TriangleMatrix<int> result = tri_matrix_1 - tri_matrix_2;
-            result.print_tri_matrix();
-            system("pause");
-
+            start_sub_tri_matrix(tri_matrix_1, size);
             break;
         }
         case 3: {
-            system("cls");
-            std::cout << "==== TRIANGULAR MATRIX MULTIPLICATION ====" << std::endl;
-
-            int size_2;
-            while (1) {
-                std::cout << "Enter the size of 2-nd matrix: ";
-                std::cin >> size_2;
-
-                if (size_1 != size_2) { //??
-                    std::cout << "Error! The sizes of the matrices must match! ";
-                    continue;
-                }
-                break;
-            }
-            TriangleMatrix<int> tri_matrix_2(size_2);
-            tri_matrix_2.input_tri_matrix();
-
-            TriangleMatrix<int> result = tri_matrix_1 * tri_matrix_2;
-            result.print_tri_matrix();
-            system("pause");
-
+            start_mult_tri_matrix(tri_matrix_1, size);
             break;
         }
         case 4: {
-            system("cls");
-            std::cout << "==== TRIANGULAR MATRIX MULTIPLICATION BY A SCALAR ====" << std::endl;
-
-            int scalar;
-            std::cout << "Enter a scalar: ";
-            std::cin >> scalar;
-
-            TriangleMatrix<int> result = tri_matrix_1 * scalar;
-            result.print_tri_matrix();
-            system("pause");
-
+            start_tri_matrix_scalar_mult(tri_matrix_1);
             break;
         }
         case 5: {
-            system("cls");
-            std::cout << "==== TRIANGULAR MATRIX TYPE ====" << std::endl;
-            tri_matrix_1.status();
-            system("pause");
+            start_type_tri_matrix(tri_matrix_1);
             break;
         }
         default:
@@ -254,7 +248,6 @@ void TriangleMatrixOperations() {
 
         }
     }
-
 }
 
 int main() {
